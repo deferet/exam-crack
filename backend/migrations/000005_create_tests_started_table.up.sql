9a0055a1855be455e8e1bbd36a07f170c045cd79
Dominik Feret <dominik.feret03@gmail.com>
2024-11-02 18:25:53 +0100

    Add migrations for creating the started tests table

---
+++ backend/migrations/000005_create_tests_started_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS tests_started (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    test_id uuid NOT NULL REFERENCES tests(id) ON DELETE CASCADE,
+    progress integer NOT NULL DEFAULT 0,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now()
+    UNIQUE (user_id, test_id)
+);
