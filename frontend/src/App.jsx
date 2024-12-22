124aa87b382b485d3f8ef29f6cb41f88fb777187
Olivier Yildiz <olivier_yildiz@o2.pl>
2024-12-22 17:34:16 +0100

    App.jsx-update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import Navbar from './components/navbar.jsx';
-import Homepage from './pages/homepage.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
+import MyTests from "./pages/MyTests";
+import Navbar from "./components/navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Homepage />
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className="bg-[#0f172a] min-h-screen text-white">
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/mytests" element={<MyTests />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
