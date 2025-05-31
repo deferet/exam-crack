e0a141015c7e99d5e82201c1425b64478a213785
Dominik Feret <dominik.feret03@gmail.com>
2025-05-31 21:13:18 +0200

    Add an endpoint for auth token creation

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -19,8 +19,9 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
-
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAutheticationTokenHandler)
 
 	// TODO - Add panic recovery middleware
 	return router
